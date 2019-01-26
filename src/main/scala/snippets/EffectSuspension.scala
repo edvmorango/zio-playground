b884372047505162ed5b758490f06126d6cbdbf8
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-26 14:58:22 -0200

    feat: effect suspension

---
+++ src/main/scala/snippets/EffectSuspension.scala
@@ -0,0 +1,30 @@
+package snippets
+
+import scalaz.zio.{App, IO}
+
+object EffectSuspension extends App {
+
+  def run(args: List[String]): IO[Nothing, ExitStatus] = {
+
+    val lazyEvaluation = IO.succeedLazy {
+      println("I'm a effect occurring in every call")
+      10
+    }
+
+    val strictEvalution = IO.succeed {
+      println("I'm a effect occurring once")
+      10
+    }
+
+    for {
+      _ <- strictEvalution
+      _ <- strictEvalution
+      _ <- strictEvalution
+      _ <- lazyEvaluation
+      _ <- lazyEvaluation
+      _ <- lazyEvaluation
+    } yield ExitStatus.ExitNow(1)
+
+  }
+
+}
